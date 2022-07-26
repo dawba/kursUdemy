ac9c49a76bafc45f8f817d0c060d8f22be38c2a1
dawba <108723343+dawba@users.noreply.github.com>
2022-07-26 15:52:10 +0200

    Delete kursUdemyProj directory

--- kursUdemyProj/konteksty/src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-const element = document.getElementById("root");
-const root = ReactDOM.createRoot(element);
-
-root.render(<App />);
